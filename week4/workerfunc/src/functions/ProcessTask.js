6f1c1b598969c81e89b30d25ab52294617b83b28
Georges <georges.boughantous@gmail.com>
2025-07-24 07:58:06 +0000

    week 6

--- week4/workerfunc/src/functions/ProcessTask.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.storageQueue('ProcessTask', {
-    queueName: 'taskqueue', // <-- match your queue name
-    connection: 'AzureWebJobsStorage', // <-- use your storage connection setting
-    handler: (queueItem, context) => {
-        context.log('✅ Processing task:', queueItem);
-    }
-});
